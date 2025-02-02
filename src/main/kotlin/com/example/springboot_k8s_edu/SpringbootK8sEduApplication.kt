a3ac5ea0ac9390c90d2210548c3f93bd8b5f6cdb
jakubchoc <jakub.choc@gmail.com>
2025-02-02 13:07:27 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/springboot_k8s_edu/SpringbootK8sEduApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springboot_k8s_edu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootK8sEduApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootK8sEduApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springboot_k8s_edu/SpringbootK8sEduApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springboot_k8s_edu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootK8sEduApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
